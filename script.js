4afe585a00d0b87ec156ec53f0d03b0aa434ad24
treaty1210 <135383414+treaty1210@users.noreply.github.com>
2023-06-29 22:19:19 -0700

    Added function to show user score on thankPage

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 let submit = document.querySelector(".submit");
 let ratePage = document.querySelector(".ratePage");
 let thankPage = document.querySelector(".thankPage");
+let ratings = document.querySelectorAll(".ratings");
+let rateScore = document.querySelector(".rateScore");
 
 //Event display to call switch on click
 submit.addEventListener("click", () => {
@@ -12,5 +14,11 @@ submit.addEventListener("click", () => {
 //function to change display
 const switchDisplay = () => {
     ratePage.style.display = "none";
-    thankPage.style.display = "block";
+    thankPage.style.display = "grid";
 }
+
+//Inserts value chosen in thank you message area
+ratings.forEach(rate => rate.addEventListener("click", (e) => {
+    rateScore.textContent = `${e.target.textContent}`;
+    console.log(`${e.target.textContent}`);
+}))
